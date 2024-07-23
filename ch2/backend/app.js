a42cadabd176f77376bef75330e691c18d8a8474
82259385 <jiwook.jeong@kt.com>
2024-07-23 19:36:43 +0900

    [fix] backend keycloak 설정 변경

--- ch2/backend/app.js
+++ ch2/backend/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var session = require('express-session');
 var Keycloak = require('keycloak-connect');
 var cors = require('cors');
+var path = require('path');
 
 var app = express();
 
@@ -16,7 +17,23 @@ app.use(session({
   store: memoryStore
 }));
 
-var keycloak = new Keycloak({ store: memoryStore });
+// keycloak.json 파일을 사용하여 설정 로드
+var keycloakConfigPath = path.join(__dirname, 'keycloak.json'); // BE 서버에서 403 access deny 적용. (책과 동일)
+// var keycloakConfigPath = path.join(__dirname, 'tmp-keycloak.json'); // BE 서버에서 realm 접속 화면으로 redirect 됨. (책과 다름)
+
+// var keycloak = new Keycloak({ store: memoryStore }, keycloakConfigPath); // 됨
+var keycloak = new Keycloak({ store: memoryStore }, keycloakConfigPath, { logLevels: ['debug'] }); // 됨
+// var keycloak = new Keycloak({ store: memoryStore }, { logLevels: ['debug'] }, keycloakConfigPath); // 안됨
+// var keycloak = new Keycloak({ store: memoryStore }, { logLevels: ['debug'] }, {config: keycloakConfig}); // 안됨
+// var keycloak = new Keycloak({ store: memoryStore }, {config: keycloakConfig}); // 안됨
+
+// Keycloak 초기화 후 설정 확인 로그
+console.log('Keycloak initialized:', keycloak);
+
+app.use(function (err, req, res, next) {
+  console.error('Error:', err);
+  res.status(err.status || 500).json({ message: err.message });
+});
 
 app.use(keycloak.middleware());
 
